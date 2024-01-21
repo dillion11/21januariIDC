15af0178a397dbc7e5ead0634c5a532f42649ede
dillion11 <dillionleywin99@gmail.com>
2024-01-21 13:13:54 +0800

    feat: implement api get question

--- scripts/app.dashboard.js
+++ scripts/app.dashboard.js
@@ -1,8 +1,42 @@
 // == TEMPLATE QUIZ ITEM ==
 
-// generateQuizItem({
-//   id: question.id,
-//   question: question.jokes,
-//   answer: question.answer,
-//   category: question.category,
-// });
+
+
+import { getQuestions, createQuestion } from "./api.js";
+import { generateQuizItem } from './utils/index.js'
+
+const quizContent = document.getElementById("quiz-content");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    async function handleAllQuestion(){
+        try {
+           const questions = await getQuestions();
+
+           //Kalo tidak ada data jangan kirim apapun
+           if(!questions) return;
+
+           quizContent.innerHTML = questions.map(
+            (question) => {
+                return generateQuizItem({
+                    id: question.id,
+                    question: question.jokes,
+                    answer: question.answer,
+                    category: question.category,
+                  });
+
+            }
+           ).join("")
+
+           //Kalo datanya ada maka lanjut
+           console.log({questions});
+            
+        } catch (error) {
+        console.error("Ada error nih : ", {
+            error,
+        });
+        }
+    }
+handleAllQuestion()
+})
+
